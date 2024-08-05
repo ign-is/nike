3812e2b45a4308e7a581bee0c27afed025860357
ign-is <igna.dev.contact@gmail.com>
2024-08-05 15:04:39 -0300

    Add nav mobile hamburger functionality

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import {hamburger } from "../assets/icons";
+import { close } from "../assets/images"
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 
 const Nav = () => {
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    }
+
     return (
         <header className="padding-x py-8 absolute z-10 w-full">
             <nav className="flex justify-between items-center max-container">
@@ -27,14 +36,49 @@ const Nav = () => {
                         </li>
                     ))}
                 </ul>
-                <div className="hidden max-lg:block">
+                {/* <div className="hidden max-lg:block">
                     <img 
                         src={hamburger} 
                         alt="Hamburger"
                         width={25}
                         height={25} 
                     />
+                </div> */}
+                <div className="lg:hidden z-30">
+                    <button onClick={toggleMobileMenu}>
+                        {isMobileMenuOpen ?
+                             <img 
+                                 src={close} 
+                                alt="Hamburger"
+                                width={25}
+                                height={25} 
+                            /> :
+                            <img 
+                                src={hamburger} 
+                                alt="Hamburger"
+                                width={25}
+                                height={25} 
+                            /> 
+                        }
+                    </button>
                 </div>
+
+                {isMobileMenuOpen && (
+                <div className="w-60 h-screen pt-24 bg-white lg:hidden absolute top-0 right-0 z-20 flex-1 flex flex-col shadow-2xl">
+                    <ul>
+                        {navLinks.map((item) => (
+                            <li key={item.label} className="mb-5">
+                                <a 
+                                    href={item.href}
+                                    className="p-4 pl-8 uppercase tracking-tighter text-xl cursor-pointer transition-all hover:font-bold"
+                                >
+                                        {item.label}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
             </nav>
         </header>
     )
